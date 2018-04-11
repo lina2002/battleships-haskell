57a560730bb726de2d81f80c7ec099cefb45b4a6
Agnieszka Paszek <apaszek@mezzobit.com>
2018-04-11 04:09:38 +0200

    Read coordinates from user

--- hello.hs
+++ hello.hs
@@ -1,4 +1,10 @@
+readCoordinates :: IO [Int]
+readCoordinates = fmap (map read) (fmap words getLine)
+
 main =  do
   putStrLn "Let's play batlleships!"
   boardSize <- readLn :: IO Int
   print boardSize
+  putStrLn "Please choose battleship coordinates"
+  coordinates <- readCoordinates
+  print coordinates
